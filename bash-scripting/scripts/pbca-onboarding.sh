22b8b6545e146c2e74cb268c4ca9bdad7c347f34
Abraham <kashuaabraham@outlook.com>
2026-08-14 22:09:31 +0100

    add PBCA staff onboarding script with file input and idempotency

--- bash-scripting/scripts/pbca-onboarding.sh
+++ bash-scripting/scripts/pbca-onboarding.sh
@@ -14,12 +14,18 @@ if [ "$EUID" -ne 0 ]; then
 	exit 1
 fi
 	while IFS= read -r STAFF; do
-		sudo useradd -m -s /bin/bash $STAFF	     
+		if  id "$STAFF" &>/dev/null ; then
+		       echo "Staff: $STAFF already exist"
+	       else
+	echo "Creating user $STAFF ...."	       
+		sudo useradd -m -s /bin/bash $STAFF
 	      if [ $? -eq 0 ];then	
 		echo "Staff: $STAFF has been created successfully"
 	else
 		echo "Staff: $STAFF creation failed"
+		exit 1
 		fi
+	fi 
 	done < $FILENAME
 
 
